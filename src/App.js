97b8927fb05808d3e5f746bdc34498b462647474
TungNX[LUV] <nghiemxuantung@luvina.net>
2020-12-22 17:43:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -14,8 +14,21 @@ import {
 function App() {
   const [defaultStyle, changeDefaultLight] = useState(true);
   const [isVisibleSidebar, changeVisibleSidebar] = useState(false);
+  const [isThemeLight, changeThemeLight] = useState(true);
+  const [valueSetting, setValueSetting] = useState
+  const theme = () => {
+    let theme;
+    if (defaultStyle) {
+      theme = "default light";
+    } else if (isThemeLight) {
+      theme = "experimental light";
+    } else {
+      theme = "experimental dark";
+    }
+    return theme;
+  }
   return (
-    <div className={defaultStyle ? "default light" : "experimental light"}>
+    <div className={theme()}>
       <div className="container">
         <ErrorBoundary>
           <Router>
@@ -25,7 +38,10 @@ function App() {
                   <Setting
                     changeDefaultLight={() => changeDefaultLight(!defaultStyle)}
                     visibleSidebar={isVisibleSidebar}
+                    isThemeLight={isThemeLight}
+                    changeThemeLight={changeThemeLight}
                     changeVisibleSidebar={() => changeVisibleSidebar(!isVisibleSidebar)}
+                    setValue={value => setValue(value)}
                     {...props}
                   />)
               }
@@ -36,6 +52,8 @@ function App() {
                   <HackerNewsSearch
                     {...props}
                     visibleSidebar={isVisibleSidebar}
+                    isThemeLight={isThemeLight}
+                    changeThemeLight={() => changeThemeLight(!isThemeLight)}
                   />
                 )
               } />
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -4,12 +4,17 @@ import Starred from './Starred';
 import Theme from './Theme';
 import '../.././assets/css/sidebar/SideBar.css';
 
-const SideBar = () => {
+const SideBar = props => {
+    console.log(props);
     return (
         <aside className="sidebar">
             <ListItem />
             <Starred />
-            <Theme />
+            <Theme
+                backgroundTheme={props.isThemeLight ? "ThemeSwitch-light" : "ThemeSwitch-dark"}
+                isThemeLight={props.isThemeLight}
+                changeThemeLight={props.changeThemeLight}
+                imageIconTheme={props.isThemeLight ? "//d3nb9u6x572n0.cloudfront.net/packs/media/components/ThemeSwitch/light-f7f5d17abc46bda6e819f67d2296052e.svg" : "//d3nb9u6x572n0.cloudfront.net/packs/media/components/ThemeSwitch/dark-8c32a1bd9064ea6ad828becaac3f603b.svg"} />
         </aside>
     );
 }
